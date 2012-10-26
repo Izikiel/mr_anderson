86c818bb16114c8fd5a09209b3bc2fd4e6a8e332
Izikiel <ezequiel.gambaccini@gmail.com>
2012-10-25 17:11:51 -0700

    Agregue para que se creen las tablas dentro de un schema, lo pide el enunciado

--- migracion.sql
+++ migracion.sql
@@ -1,9 +1,13 @@
-
+USE [GD2C2012]
+GO
+/****** Object:  Schema [gd_esquema]    Script Date: 04/15/2012 01:58:54 ******/
+CREATE SCHEMA [MR_ANDERSON] AUTHORIZATION [gd]
+GO
 --Creacion de tablas
 
-begin tran trn_creacion_tablas
+begin transaction trn_creacion_tablas
 
-	create table Datos_Clientes
+	create table MR_ANDERSON.Datos_Clientes
 	(
 		dni			numeric(18,0) primary key,	
 		nombre		nvarchar(255),
@@ -16,7 +20,7 @@ begin tran trn_creacion_tablas
 
 	)
 
-	create table Cupones
+	create table MR_ANDERSON.Cupones
 	(
 		codigo 			 nvarchar(50) primary key,
 		precio			 numeric(18,2),
@@ -26,7 +30,7 @@ begin tran trn_creacion_tablas
 	)
 
 
-	create table Cupones_Transacciones
+	create table MR_ANDERSON.Cupones_Transacciones
 	(
 		codigo 			 nvarchar(50),
 		fecha 			 datetime,
@@ -35,30 +39,30 @@ begin tran trn_creacion_tablas
 		fecha_devolucion datetime,
 		fecha_entregado  datetime,
 
-		foreign key (codigo) references dbo.Cupones(codigo)
+		foreign key (codigo) references MR_ANDERSON.Cupones(codigo)
 
 	)
 
 
-	create table Giftcard
+	create table MR_ANDERSON.Giftcard
 	(
 		fecha 		datetime,
 		monto		numeric(18,2),
-		cliente_origen numeric(18,0) constraint dni1 foreign key  references dbo.Datos_Clientes(dni),
-		cliente_destino numeric(18,0) constraint dni2 foreign key  references dbo.Datos_Clientes(dni)
+		cliente_origen numeric(18,0) constraint dni1 foreign key  references MR_ANDERSON.Datos_Clientes(dni),
+		cliente_destino numeric(18,0) constraint dni2 foreign key  references MR_ANDERSON.Datos_Clientes(dni)
 
 	)
 
-	create table Cargas
+	create table MR_ANDERSON.Cargas
 	(
 		dni numeric(18,0),
 		carga_credito numeric(18,2),
 		carga_fecha datetime,
 
-		foreign key (dni) references dbo.Datos_Clientes(dni)
+		foreign key (dni) references MR_ANDERSON.Datos_Clientes(dni)
 	)
 
-	create table Datos_Proveedores (
+	create table MR_ANDERSON.Datos_Proveedores (
 
 		provee_cuit nvarchar(20),
 		provee_rs nvarchar(100),
@@ -69,7 +73,7 @@ begin tran trn_creacion_tablas
 		primary key(provee_cuit,provee_rs)
 	)
 
-	create table Factura (
+	create table MR_ANDERSON.Factura (
 
 		factura_nro numeric(18,0) PRIMARY KEY,
 		factura_fecha datetime,
@@ -77,20 +81,20 @@ begin tran trn_creacion_tablas
 		fecha_fin_periodo datetime,
 		provee_cuit nvarchar(20),
 		provee_rs nvarchar(100),
-		FOREIGN KEY (provee_cuit,provee_rs) references Datos_Proveedores(provee_cuit,provee_rs)
+		FOREIGN KEY (provee_cuit,provee_rs) references MR_ANDERSON.Datos_Proveedores(provee_cuit,provee_rs)
 	)
 
-	create table Renglon_Factura (
+	create table MR_ANDERSON.Renglon_Factura (
 
 		factura_nro numeric(18,0),
 		cupon nvarchar(50),
-		FOREIGN KEY (factura_nro) references Factura(factura_nro),
-		FOREIGN KEY (cupon) references Cupones(codigo),
+		FOREIGN KEY (factura_nro) references MR_ANDERSON.Factura(factura_nro),
+		FOREIGN KEY (cupon) references MR_ANDERSON.Cupones(codigo),
 		cantidad numeric(18,0)
 
 	)
 
-	create table Credito (
+	create table MR_ANDERSON.Credito (
 
 		cli_dni numeric(18,0) PRIMARY KEY,
 		valor_credito numeric(18,2),
@@ -99,10 +103,10 @@ begin tran trn_creacion_tablas
 	)
 
 
-	create table Login (
+	create table MR_ANDERSON.Login (
 
 		username nvarchar(20) PRIMARY KEY,
-		user_password nvarchar(64),
+		user_password nvarchar(255),
 		lastlogin datetime,
 		rol_asignado numeric(18,0),
 		intentos_fallidos numeric(18,0),
@@ -110,21 +114,21 @@ begin tran trn_creacion_tablas
 
 	)
 
-commit tran trn_creacion_tablas
+commit transaction trn_creacion_tablas
 
---- Termina la creacion de tablas, arranca el relleno de datos
+--- Termina la creacion de tablas, arranca el relleno 	de datos
 
 
 begin tran trn_inserts_tablas
 
-	insert into Datos_Clientes (dni, nombre, apellido, direccion, telefono, mail, fecha_nac, ciudad)
+	insert into MR_ANDERSON.Datos_Clientes (dni, nombre, apellido, direccion, telefono, mail, fecha_nac, ciudad)
 		
 		select distinct master.Cli_Dni, master.Cli_Nombre, master.Cli_Apellido, 
 			   master.Cli_Direccion, master.Cli_Telefono, master.Cli_Mail, master.Cli_Fecha_Nac, master.Cli_Ciudad
 			from gd_esquema.Maestra master 
 
 
-	insert into Cupones(codigo, precio, precio_fict, cantidad, descripcion)
+	insert into MR_ANDERSON.Cupones(codigo, precio, precio_fict, cantidad, descripcion)
 		
 		select distinct master.Groupon_Codigo, master.Groupon_Precio, master.Groupon_Precio_Ficticio,
 				master.Groupon_Cantidad, master.Groupon_Descripcion
@@ -132,7 +136,7 @@ begin tran trn_inserts_tablas
 			where master.Groupon_Codigo is not null
 
 
-	insert into Cupones_Transacciones(codigo,fecha,fecha_venc,
+	insert into MR_ANDERSON.Cupones_Transacciones(codigo,fecha,fecha_venc,
 									  fecha_compra,fecha_devolucion,fecha_entregado)
 			select master.Groupon_Codigo,master.Groupon_Fecha,
 			 	   master.Groupon_Fecha_Venc, master.Groupon_Fecha_Compra,
@@ -140,7 +144,7 @@ begin tran trn_inserts_tablas
 				from gd_esquema.Maestra master
 				
 
-	insert into Giftcard(fecha, monto, cliente_origen,cliente_destino)
+	insert into MR_ANDERSON.Giftcard(fecha, monto, cliente_origen,cliente_destino)
 
 		select master.GiftCard_Fecha, master.GiftCard_Monto,
 		 		master.Cli_Dni, master.Cli_Dest_Dni
@@ -150,13 +154,13 @@ begin tran trn_inserts_tablas
 
 
 
-	insert into Datos_Proveedores(provee_cuit,provee_rs,provee_dom,provee_ciudad,provee_telefono,provee_rubro)
+	insert into MR_ANDERSON.Datos_Proveedores(provee_cuit,provee_rs,provee_dom,provee_ciudad,provee_telefono,provee_rubro)
 		select distinct master.Provee_CUIT,master.Provee_RS,master.Provee_Dom,master.Provee_Ciudad,master.Provee_Telefono,master.Provee_Rubro
 		from gd_esquema.Maestra master
 		where master.Provee_CUIT is not null
 
 
-	insert into Cargas(dni,carga_credito,carga_fecha)
+	insert into MR_ANDERSON.Cargas(dni,carga_credito,carga_fecha)
 		select master.Cli_Dni,master.carga_credito,master.carga_fecha
 		from gd_esquema.Maestra master
 		where master.Cli_Dni is not null and master.carga_credito is not null and master.carga_fecha is not null
@@ -167,17 +171,17 @@ begin tran trn_inserts_tablas
 
 --Testing (FALTA)
 
-	--insert into Credito(cli_dni,valor_credito,fecha_ultima_cargada)
+	--insert into MR_ANDERSON.Credito(cli_dni,valor_credito,fecha_ultima_cargada)
 		--select master.Cli_Dni
 
-	--insert into Factura()
+	--insert into MR_ANDERSON.Factura()
 	--	select TOP 100 Factura_Nro, Factura_Fecha, Groupon_Fecha_Compra, Provee_RS, Provee_CUIT
 	--from gd_esquema.Maestra
 	--where Factura_Nro is not null and Factura_Fecha is not null
 	--order by Factura_Nro
 
-	--insert into Renglon_Factura()
+	--insert into MR_ANDERSON.Renglon_Factura()
 
-	--insert into login()
+	--insert into MR_ANDERSON.login()
 
 commit tran trn_inserts_tablas
