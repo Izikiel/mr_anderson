6228e8075a9ac9da27909a8be58b8e04268402f1
rodricad <rodricand@gmail.com>
2012-12-04 20:48:09 -0800

    Agregar ComprarGiftCard

---
+++ Aplicacion Desktop/GrouponDesktop/UI/ComprarGiftCard.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GrouponDesktop.UI
+{
+    partial class ComprarGiftCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBoxUsuarioDestino = new System.Windows.Forms.TextBox();
+            this.TxtBoxMontoGift = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblSaldo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtBoxUsuarioDestino
+            // 
+            this.TxtBoxUsuarioDestino.Location = new System.Drawing.Point(128, 42);
+            this.TxtBoxUsuarioDestino.Name = "TxtBoxUsuarioDestino";
+            this.TxtBoxUsuarioDestino.Size = new System.Drawing.Size(100, 20);
+            this.TxtBoxUsuarioDestino.TabIndex = 1;
+            // 
+            // TxtBoxMontoGift
+            // 
+            this.TxtBoxMontoGift.Location = new System.Drawing.Point(128, 92);
+            this.TxtBoxMontoGift.Name = "TxtBoxMontoGift";
+            this.TxtBoxMontoGift.Size = new System.Drawing.Size(100, 20);
+            this.TxtBoxMontoGift.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Monto de GiftCard";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Usuario Destino";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Su saldo es de:";
+            // 
+            // LblSaldo
+            // 
+            this.LblSaldo.AutoSize = true;
+            this.LblSaldo.Location = new System.Drawing.Point(216, 9);
+            this.LblSaldo.Name = "LblSaldo";
+            this.LblSaldo.Size = new System.Drawing.Size(35, 13);
+            this.LblSaldo.TabIndex = 6;
+            this.LblSaldo.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Comprar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ComprarGiftCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 176);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LblSaldo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtBoxMontoGift);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtBoxUsuarioDestino);
+            this.Name = "ComprarGiftCard";
+            this.Text = "ComprarGiftCard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBoxUsuarioDestino;
+        private System.Windows.Forms.TextBox TxtBoxMontoGift;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblSaldo;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
---
+++ Aplicacion Desktop/GrouponDesktop/UI/ComprarGiftCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GrouponDesktop.UI
+{
+    public partial class ComprarGiftCard : Form
+    {
+        LoginWindow login;
+        String saldo;
+        public ComprarGiftCard(LoginWindow login)
+        {
+            this.login = login;
+            saldo = login.UsuarioActivo.DatosCliente.Saldo.ToString();
+            if (String.IsNullOrEmpty(saldo))
+                LblSaldo.Text = "0";
+            else
+                LblSaldo.Text = saldo;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User.HomeUsuarios usuarios = new GrouponDesktop.User.HomeUsuarios();
+            int montoGiftCard = 0;
+            if(Int32.TryParse(TxtBoxMontoGift.Text, out montoGiftCard) == false)
+                return;
+            if (usuarios.usuarioNoExistente(TxtBoxUsuarioDestino.Text) || montoGiftCard > Int32.Parse(saldo))
+            {
+                return;
+            }
+            //Do SP
+        }
+
+
+    }
+}
