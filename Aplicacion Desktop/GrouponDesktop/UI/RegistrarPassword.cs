2d43733b8e6d716d5707ed9262bffa05dd34f028
Izikiel <ezequiel.gambaccini@gmail.com>
2012-12-09 09:58:37 -0800

    aca tenes los RegistrarPassword

---
+++ Aplicacion Desktop/GrouponDesktop/UI/RegistrarPassword.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GrouponDesktop.DataAdapter;
+
+namespace GrouponDesktop.UI
+{
+    public partial class RegistrarPassword : Form
+    {
+        String username;
+        public RegistrarPassword(Login log)
+        {
+            username = log.UserName;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.text_pass.Text.Length < 8)
+                MessageBox.Show("La longitud de la contraseña debe ser de al menos 8 caracteres", "Error");
+            else
+            {
+                Login login = new Login();
+                login.UserName = this.username;
+                login.Password = this.text_pass.Text;
+                login.setPasswordOldUser();
+                this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
