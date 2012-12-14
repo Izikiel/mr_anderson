667f04df1d98ba90b9f5037edfdca3db7749df4c
Izikiel <ezequiel.gambaccini@gmail.com>
2012-12-14 09:46:51 -0800

    modificado el historial

--- creacion_tablas_en_esquema.sql
+++ creacion_tablas_en_esquema.sql
@@ -1500,8 +1500,15 @@ create procedure MR_ANDERSON.historial_compra (@dni numeric(18,0), @fecha_inicio
     as
         begin
             select codigo, C.id_compra,'COMPRADO' as Estado 
-                from MR_ANDERSON.Compras C 
+                from MR_ANDERSON.Compras C
+
+                join MR_ANDERSON.Cupones Cupones
+                    on   C.codigo = Cupones.codigo 
+
                 where C.dni = @dni and C.fecha >= @fecha_inicio and C.fecha <= @fecha_final
+                    and C.id_compra not in (select id_compra from MR_ANDERSON.Devoluciones)
+                    and C.id_compra not in (select id_compra from MR_ANDERSON.Consumos)
+                    and Cupones.vencimiento_canje >= @fecha_final 
 
             union select codigo, D.id_compra,'DEVUELTO' as Estado 
                 from MR_ANDERSON.Compras Compras
@@ -1510,6 +1517,25 @@ create procedure MR_ANDERSON.historial_compra (@dni numeric(18,0), @fecha_inicio
                     on   Compras.id_compra = D.id_compra
                 
                 where Compras.dni = @dni and D.fecha_devolucion >= @fecha_inicio and D.fecha_devolucion <= @fecha_final
+
+            union select codigo, Consumos.id_compra, 'Consumido' as Estado 
+                from MR_ANDERSON.Compras Compras
+
+                join MR_ANDERSON.Consumos Consumos
+                    on   Compras.condition = Consumos.id_compra
+
+                where Compras.dni = @dni and Consumos.fecha_consumo >= @fecha_inicio and Consumos.fecha_consumo <= @fecha_final
+
+            union select codigo, Compras.id_compra, 'Vencido' as Estado 
+                from MR_ANDERSON.Compras Compras
+
+                join MR_ANDERSON.Cupones Cupones
+                    on   Compras.codigo = Cupones.codigo
+                
+                where Compras.dni = @dni and Compras.fecha >= @fecha_inicio and Compras.fecha <= @fecha_final
+                    and Compras.id_compra not in (select id_compra from MR_ANDERSON.Devoluciones)
+                    and Compras.id_compra not in (select id_compra from MR_ANDERSON.Consumos)
+                    and Cupones.vencimiento_canje <= @fecha_final 
 
             order by Estado
         end
